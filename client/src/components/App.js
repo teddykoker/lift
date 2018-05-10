9e449c4ef18ac37679404a85ba18f6dd42140a84
teddykoker <git@tomkoker.com>
2018-05-09 21:19:48 -0400

    Added tachyons css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,31 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <h1>Lift</h1>;
+    return (
+      <nav class="pa3 pa4-ns">
+        <a
+          class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
+          href="#"
+          title="Home"
+        >
+          Lift
+        </a>
+        <div class="tc pb3">
+          <a class="link dim gray f6 f5-ns dib mr3" href="#">
+            Programs
+          </a>
+          <a class="link dim gray f6 f5-ns dib mr3" href="#" title="About">
+            About
+          </a>
+          <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Store">
+            Sign Up
+          </a>
+          <a class="link dim gray f6 f5-ns dib" href="#" title="Contact">
+            Log In
+          </a>
+        </div>
+      </nav>
+    );
   }
 }
 
