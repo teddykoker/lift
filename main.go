ecbb7389af7dbcd017bef20b4350cf78acc2ede8
teddykoker <git@tomkoker.com>
2018-05-12 14:20:16 -0400

    run again

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"lift/models"
 	"log"
 	"os"
 
@@ -20,48 +19,48 @@ func main() {
 	log.Printf("Listening on %s", addr)
 
 	app := NewApp(os.Getenv("DATABASE_URL"))
-	//app.Run(addr)
+	app.Run(addr)
 
-	store := app.Store
+	// store := app.Store
 
-	store.Exercises.Init()
+	// store.Exercises.Init()
 
-	err := store.Exercises.Insert(&models.Exercise{
-		Reps:      5,
-		Sets:      5,
-		Weight:    315.0,
-		Movement:  "Squat",
-		Sequence:  0,
-		ProgramID: 1,
-	})
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// err := store.Exercises.Insert(&models.Exercise{
+	// 	Reps:      5,
+	// 	Sets:      5,
+	// 	Weight:    315.0,
+	// 	Movement:  "Squat",
+	// 	Sequence:  0,
+	// 	ProgramID: 1,
+	// })
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	exercises, err := store.Exercises.List()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(exercises)
+	// exercises, err := store.Exercises.List()
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// fmt.Println(exercises)
 
-	dude := &models.User{
-		Username: "dude",
-		Password: "password",
-	}
-	store.Users.Init()
-	err = store.Users.Insert(dude)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Print(*dude)
-	}
+	// dude := &models.User{
+	// 	Username: "dude",
+	// 	Password: "password",
+	// }
+	// store.Users.Init()
+	// err = store.Users.Insert(dude)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Print(*dude)
+	// }
 
-	dude.Password = "password"
-	err = store.Users.Authenticate(dude)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("User Authenticated")
-	}
+	// dude.Password = "password"
+	// err = store.Users.Authenticate(dude)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println("User Authenticated")
+	// }
 
 }
