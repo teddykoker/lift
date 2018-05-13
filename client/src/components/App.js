dcdd5f58011ae1ceb34f6cd5e980ecc73765be54
teddykoker <git@tomkoker.com>
2018-05-12 22:43:36 -0400

    added redux and basic app_loaded state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
+import api, { setToken } from "../api";
+import { onLoad } from "../actions";
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: token => dispatch(onLoad(token))
+});
 
 class App extends React.Component {
+  componentWillMount() {
+    const token = window.localStorage.getItem("jwt");
+    if (token) {
+      api.setToken(token);
+    }
+    this.props.onLoad(token);
+  }
+
   render() {
     return (
-      <nav class="pa3 pa4-ns">
+      <nav className="pa3 pa4-ns">
         <a
-          class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
+          className="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
           href="#"
           title="Home"
         >
           Lift
         </a>
-        <div class="tc pb3">
-          <a class="link dim gray f6 f5-ns dib mr3" href="#">
+        <div className="tc pb3">
+          <a className="link dim gray f6 f5-ns dib mr3" href="#">
             Programs
           </a>
-          <a class="link dim gray f6 f5-ns dib mr3" href="#" title="About">
+          <a className="link dim gray f6 f5-ns dib mr3" href="#" title="About">
             About
           </a>
-          <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Store">
+          <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Store">
             Sign Up
           </a>
-          <a class="link dim gray f6 f5-ns dib" href="#" title="Contact">
+          <a className="link dim gray f6 f5-ns dib" href="#" title="Contact">
             Log In
           </a>
         </div>
@@ -30,4 +47,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import "tachyons/css/tachyons.css";
 import App from "./components/App";
+import reducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  reducer /* preloadedState, */,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
